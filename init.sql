82689da218dd4d7090b80cefe77da65dd1f1798a
Fernando <fernandocastrejonh@gmail.com>
2025-04-18 09:18:04 -0600

    changes

---
+++ init.sql
@@ -0,0 +1,15 @@
+CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'Auth123';
+
+CREATE DATABASE auth;
+
+GRANT ALL PRIVILEDES ON auth.* TO 'auth_user'@'localhost';
+
+USE auth;
+
+CREATE TABLE user (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+INSERT INTO user (email, password) VALUES ('fernando@gmail.com', 'admin123')
